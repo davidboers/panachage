a8281af23e72e7da09261185ee447858be1d9e72
davidboers <kwmmineccraft@gmail.com>
2024-12-22 15:46:25 -0500

    full and free list tests

--- tests/votes.cpp
+++ tests/votes.cpp
@@ -9,18 +9,45 @@
 
 using namespace panachage;
 
-int main(void) {
-    partylist listA = new_partylist(1, "Party A", {101, 102, 103, 104, 105, 106}); 
-    partylist listB = new_partylist(2, "Party B", {201, 202, 203, 204, 205, 206}); 
-    std::vector<partylist*> lists = {&listA, &listB};
+int main(void)
+{
+    partylist listA = new_partylist(1, "Party A", {101, 102, 103, 104, 105, 106});
+    partylist listB = new_partylist(2, "Party B", {201, 202, 203, 204, 205, 206});
+    std::vector<partylist *> lists = {&listA, &listB};
 
     PartialListVote partial(&listA, {101, 103}, {104, 201});
-    if (partial.validate()) {
+    if (partial.validate())
+    {
         std::cout << "Partial vote is valid." << std::endl;
         partial.count(lists);
         displayAllCandidates(lists);
-    } else {
+    }
+    else
+    {
         std::cout << "Partial vote is not valid." << std::endl;
     }
-    
+
+    FullListVote full(&listB);
+    if (full.validate())
+    {
+        std::cout << "Full vote is valid." << std::endl;
+        full.count(lists);
+        displayAllCandidates(lists);
+    }
+    else
+    {
+        std::cout << "Full vote is not valid." << std::endl;
+    }
+
+    FreeVote free({101, 102, 204});
+    if (free.validate())
+    {
+        std::cout << "Free vote is valid." << std::endl;
+        free.count(lists);
+        displayAllCandidates(lists);
+    }
+    else
+    {
+        std::cout << "Free vote is not valid." << std::endl;
+    }
 }
