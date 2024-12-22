43b9ed4007d67edfd70dba27eec518f18a49de4f
davidboers <kwmmineccraft@gmail.com>
2024-12-22 12:12:45 -0500

    Added free list votes

---
+++ free_list.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <bits/stdc++.h>
+
+#include "vote.hpp"
+#include "candidate.hpp"
+#include "settings.h"
+
+namespace panachage
+{
+
+    class FreeVote : public Vote
+    {
+        std::vector<candidate::id_type> votes;
+
+        void count(std::vector<partylist> lists)
+        {
+            for (candidate::id_type cand_id : votes)
+            {
+                for (partylist plistv : lists)
+                {
+                    partylist *plist = &plistv;
+                    std::vector<candidate::id_type> candidates = candidateList(plist);
+                    if (!!std::count(candidates.begin(), candidates.end(), cand_id))
+                    {
+                        plist->candidate_votes[cand_id]++;
+                    }
+                }
+            }
+        }
+
+        bool validate()
+        {
+            return votes.size() <= seats;
+        }
+    };
+}
