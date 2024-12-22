a5b4b8b2f365c49f074bdca25c70d74eef482b14
davidboers <kwmmineccraft@gmail.com>
2024-12-22 14:58:32 -0500

    party list constructor

--- partylist.cpp
+++ partylist.cpp
@@ -4,19 +4,32 @@
 
 #include "candidate.hpp"
 
+#pragma once
+
 namespace panachage
 {
 
-    typedef struct
+    struct partylist
     {
         typedef unsigned int id_type;
 
         const id_type id;
         const std::string name;
-        int list_votes;
+        int at_large_votes;
         std::map<candidate::id_type, int> candidate_votes;
+    };
 
-    } partylist;
+    partylist new_partylist(const partylist::id_type id,
+                            const std::string name,
+                            const std::vector<candidate::id_type> candidates)
+    {
+        std::map<candidate::id_type, int> candidate_votes;
+        for (candidate::id_type cand_id : candidates)
+        {
+            candidate_votes[cand_id] = 0;
+        }
+        return partylist{id, name, 0, candidate_votes};
+    }
 
     std::vector<candidate::id_type> candidateList(partylist *plist)
     {
