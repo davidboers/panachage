f74d890d5c604d3eeeb083bf5f09ae8d9e658e07
davidboers <kwmmineccraft@gmail.com>
2024-12-24 14:53:07 -0500

    test results

--- tests/votefile.cpp
+++ tests/votefile.cpp
@@ -28,4 +28,15 @@ int main(void)
     }
     displayAllCandidates(lists);
     std::cout << invalid << " invalid votes.";
+
+    /* | List: 1   ALV: 0        | List: 2   ALV: 0
+       | 1   | 101   | 1     |   | 1   | 201   | 2     |
+       | 2   | 102   | 2     |   | 2   | 202   | 1     |
+       | 3   | 103   | 0     |   | 3   | 203   | 1     |
+       | 4   | 104   | 2     |   | 4   | 204   | 2     |
+       | 5   | 105   | 1     |   | 5   | 205   | 1     |
+       | 6   | 106   | 1     |   | 6   | 206   | 1     |
+       | Party total | 7     |   | Party total | 8     |
+       0 invalid votes.
+    */
 }
