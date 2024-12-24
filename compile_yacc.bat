166996088b51067f7231e3eeb3883fb25eaf9dc5
davidboers <kwmmineccraft@gmail.com>
2024-12-23 23:41:47 -0500

    improved

--- compile_yacc.bat
+++ compile_yacc.bat
@@ -1,3 +1,4 @@
+@echo off
 flex -o partylist_lexer.c partylist.l
 flex -o votes_lexer.c votes.l
 bison -dy partylist.y -b partylist
@@ -8,9 +9,21 @@ goto Complete
 
 :Test
 cd tests
+
+echo Compiling votefile.cpp...
 g++ votefile.cpp -o votefile
+echo Executing votefile.exe...
 votefile
+echo Error level:
 echo %ERRORLEVEL%
+
+echo Compiling partylistfile.cpp...
+g++ partylistfile.cpp -o partylistfile
+echo Executing partylistfile.exe...
+partylistfile
+echo Error level:
+echo %ERRORLEVEL%
+
 cd ..
 
 :Complete
