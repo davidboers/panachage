ac5b28f0acac64fc1b6374521d5ed0a9e8c21b47
davidboers <kwmmineccraft@gmail.com>
2024-12-26 17:20:23 -0500

    Abstractify lex definitions

--- lex_utils.h
+++ lex_utils.h
@@ -4,7 +4,16 @@
 
 int yycolumn = 1;
 
-#ifndef YY_MAX_CANDS
 #define YY_MAX_CANDS 32
-#endif
-#define YY_USER_ACTION yycolumn+=yyleng;
+
+#define YY_USER_ACTION yycolumn += yyleng;
+
+#define YY_MAKE_INT(intval)     \
+    intval = std::atoi(yytext); \
+    return number;
+
+#define YY_MAKE_SELF return yytext[0];
+
+#define YY_RESET_COLUMN yycolumn = 1;
+
+#define YY_END_FILE yyterminate();
