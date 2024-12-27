1b5e1cc5a02a523387c16020c9745bf58061f1e1
davidboers <kwmmineccraft@gmail.com>
2024-12-27 16:47:06 -0500

    reworked

--- tests/partylistfile.cpp
+++ tests/partylistfile.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <iomanip>
+#include <iostream>
 
 #define JUST_PARTYLIST
 
@@ -6,11 +8,41 @@
 
 using namespace panachage;
 
+void test_pl_file(const char *path)
+{
+    partylist pl = parsePartyListFile(path);
+
+    std::cout << "Successfully parsed " << std::string(path) << std::endl;
+    std::cout << std::setw(5) << pl.name
+              << std::setw(5) << pl.id
+              << std::setw(10) << pl.at_large_votes
+              << std::setw(5) << pl.candidate_votes.size()
+              << std::endl
+              << std::endl;
+}
+
 int main(void)
 {
-    std::vector<partylist *> lists = {parsePartyListFile("liechtenstein/vu.txt"),
-                                      parsePartyListFile("liechtenstein/fbp.txt"),
-                                      parsePartyListFile("liechtenstein/fl.txt"),
-                                      parsePartyListFile("liechtenstein/dpl.txt"),
-                                      parsePartyListFile("liechtenstein/du.txt")};
+    test_pl_file("liechtenstein/vu.txt");
+    test_pl_file("liechtenstein/fbp.txt");
+    test_pl_file("liechtenstein/fl.txt");
+    test_pl_file("liechtenstein/dpl.txt");
+    test_pl_file("liechtenstein/du.txt");
+
+    /*
+    Successfully parsed liechtenstein/vu.txt
+       vu    1      9404   14
+
+    Successfully parsed liechtenstein/fbp.txt
+      fbp    2     12480   12
+
+    Successfully parsed liechtenstein/fl.txt
+       fl    3      8250    8
+
+    Successfully parsed liechtenstein/dpl.txt
+      dpl    4      6143    8
+
+    Successfully parsed liechtenstein/du.txt
+       du    5      4793    3
+    */
 }
