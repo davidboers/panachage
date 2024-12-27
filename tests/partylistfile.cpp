9c5b509b58c4aba32cf433a84fee9150e93141e6
davidboers <kwmmineccraft@gmail.com>
2024-12-27 17:06:25 -0500

    abstractified

--- tests/partylistfile.cpp
+++ tests/partylistfile.cpp
@@ -8,26 +8,42 @@
 
 using namespace panachage;
 
-void test_pl_file(const char *path)
+#ifdef NOMAIN
+#define TEST_VERBOSE false
+#else
+#define TEST_VERBOSE true
+#endif
+
+partylist test_pl_file(const char *path, bool verbose = TEST_VERBOSE)
 {
     partylist pl = parsePartyListFile(path);
 
-    std::cout << "Successfully parsed " << std::string(path) << std::endl;
-    std::cout << std::setw(5) << pl.name
-              << std::setw(5) << pl.id
-              << std::setw(10) << pl.at_large_votes
-              << std::setw(5) << pl.candidate_votes.size()
-              << std::endl
-              << std::endl;
+    if (verbose)
+    {
+        std::cout << "Successfully parsed " << std::string(path) << std::endl;
+        std::cout << std::setw(5) << pl.name
+                  << std::setw(5) << pl.id
+                  << std::setw(10) << pl.at_large_votes
+                  << std::setw(5) << pl.candidate_votes.size()
+                  << std::endl
+                  << std::endl;
+    }
+
+    return pl;
 }
 
+std::vector<partylist> test_all_pls()
+{
+    return {test_pl_file("liechtenstein/vu.txt"),
+            test_pl_file("liechtenstein/fbp.txt"),
+            test_pl_file("liechtenstein/fl.txt"),
+            test_pl_file("liechtenstein/dpl.txt"),
+            test_pl_file("liechtenstein/du.txt")};
+}
+
+#ifndef NOMAIN
 int main(void)
 {
-    test_pl_file("liechtenstein/vu.txt");
-    test_pl_file("liechtenstein/fbp.txt");
-    test_pl_file("liechtenstein/fl.txt");
-    test_pl_file("liechtenstein/dpl.txt");
-    test_pl_file("liechtenstein/du.txt");
 
     /*
     Successfully parsed liechtenstein/vu.txt
@@ -46,3 +62,4 @@ int main(void)
        du    5      4793    3
     */
 }
+#endif
