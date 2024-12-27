2bb9af73f18ceb51dc02b1def45f994967387cf0
davidboers <kwmmineccraft@gmail.com>
2024-12-27 18:27:59 -0500

    ids and party seat getter

--- seat_alloc_utils.c
+++ seat_alloc_utils.c
@@ -1,17 +1,36 @@
 #pragma once
 
+#ifndef party_id_type
+#define party_id_type unsigned int
+#endif
+
 typedef struct
 {
+    party_id_type id;
     int votes;
     int seats;
 } party;
 
-party party_new(int votes)
+party party_new(const party_id_type id, int votes)
 {
     party p;
+    p.id = id;
     p.votes = votes;
     p.seats = 0;
     return p;
+}
+
+int party_get_seats(const int partyc, party *parties, const party_id_type id)
+{
+    for (int i = 0; i < partyc; i++)
+    {
+        party p = parties[i];
+        if (p.id == id)
+        {
+            return p.seats;
+        }
+    }
+    return -1;
 }
 
 int total_valid_poll(const int partyc, party *parties)
