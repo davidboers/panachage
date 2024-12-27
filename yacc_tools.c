7902f48b09bd955c300772b32bfb263c00f47196
davidboers <kwmmineccraft@gmail.com>
2024-12-26 21:22:26 -0500

    Fixed line peeks

--- yacc_tools.c
+++ yacc_tools.c
@@ -1,32 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+extern int yylineno;
+extern int yycolumn;
+extern char *yytext;
+extern int yyleng;
+extern FILE *yyin;
+
 void yacc_error(const char *filename, char const *s, const char *t)
 {
-    extern int yylineno;
-    extern int yycolumn;
-    extern char *yytext;
     const int expr_size = 255;
     char expr[expr_size];
-    FILE *file = fopen(filename, "r");
+    rewind(yyin);
     for (int l = 1; l <= yylineno; l++)
     {
-        fgets(expr, expr_size, file);
+        fgets(expr, expr_size, yyin);
         char *lb = strchr(expr, '\n');
         if (lb)
             *lb = 0;
     }
+    fclose(yyin);
+
+    char *e = strstr(expr, yytext);
+    int error_begin;
+    if (e != NULL)
+    {
+        error_begin = (int)(e - expr) + 1;
+    }
+    else
+    {
+        error_begin = yycolumn;
+    }
 
     printf("Error parsing %s file:\n", t);
-    printf("%s:%i:%i: %s\n", filename, yylineno, yycolumn, s);
-    printf("%i: %s\n", yylineno, expr);
+    printf("%s:%i:%i: %s\n", filename, yylineno, error_begin, s);
+    printf("%-3i | %s\n", yylineno, expr);
 
-    char linenumstr[10];
-    sprintf(linenumstr, "%s", yylineno);
-    const int num_wspc = 2 + strlen(linenumstr) + (yycolumn - 1);
-    for (int i = 0; i < num_wspc; i++)
-        printf(" ");
-    printf("^\n");
+    const int extra_spc = 6;
+    const int x = error_begin + yyleng;
+    for (int i = -extra_spc; i < x; i++)
+    {
+        if (i == 0)
+        {
+            continue;
+        }
+        else if (i < error_begin)
+        {
+            printf(" ");
+        }
+        else if (i == error_begin)
+        {
+            printf("^");
+        }
+        else
+        {
+            printf("~");
+        }
+    }
+
+    printf("\n");
 
     printf("Char dumps (yytext): ");
     char h;
