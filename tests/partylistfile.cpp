3c26cf8d28c87823a449452d471a9db21990c35e
davidboers <kwmmineccraft@gmail.com>
2024-12-30 14:25:35 -0500

    Test now actually runs

--- tests/partylistfile.cpp
+++ tests/partylistfile.cpp
@@ -16,50 +16,50 @@ using namespace panachage;
 
 partylist test_pl_file(const char *path, bool verbose = TEST_VERBOSE)
 {
-    partylist pl = parsePartyListFile(path);
+  partylist pl = parsePartyListFile(path);
 
-    if (verbose)
-    {
-        std::cout << "Successfully parsed " << std::string(path) << std::endl;
-        std::cout << std::setw(5) << pl.name
-                  << std::setw(5) << pl.id
-                  << std::setw(10) << pl.at_large_votes
-                  << std::setw(5) << pl.candidate_votes.size()
-                  << std::endl
-                  << std::endl;
-    }
+  if (verbose)
+  {
+    std::cout << "Successfully parsed " << std::string(path) << std::endl;
+    std::cout << std::setw(5) << pl.name
+              << std::setw(5) << pl.id
+              << std::setw(10) << pl.at_large_votes
+              << std::setw(5) << pl.candidate_votes.size()
+              << std::endl
+              << std::endl;
+  }
 
-    return pl;
+  return pl;
 }
 
 std::vector<partylist> test_all_pls()
 {
-    return {test_pl_file("liechtenstein/vu.txt"),
-            test_pl_file("liechtenstein/fbp.txt"),
-            test_pl_file("liechtenstein/fl.txt"),
-            test_pl_file("liechtenstein/dpl.txt"),
-            test_pl_file("liechtenstein/du.txt")};
+  return {test_pl_file("liechtenstein/vu.txt"),
+          test_pl_file("liechtenstein/fbp.txt"),
+          test_pl_file("liechtenstein/fl.txt"),
+          test_pl_file("liechtenstein/dpl.txt"),
+          test_pl_file("liechtenstein/du.txt")};
 }
 
 #ifndef NOMAIN
 int main(void)
 {
+  test_all_pls();
+  /*
+  Successfully parsed liechtenstein/vu.txt
+    vu    1      9404   14
 
-    /*
-    Successfully parsed liechtenstein/vu.txt
-       vu    1      9404   14
+  Successfully parsed liechtenstein/fbp.txt
+    fbp    2     12480   12
 
-    Successfully parsed liechtenstein/fbp.txt
-      fbp    2     12480   12
+  Successfully parsed liechtenstein/fl.txt
+    fl    3      8250    8
 
-    Successfully parsed liechtenstein/fl.txt
-       fl    3      8250    8
+  Successfully parsed liechtenstein/dpl.txt
+    dpl    4      6143    8
 
-    Successfully parsed liechtenstein/dpl.txt
-      dpl    4      6143    8
-
-    Successfully parsed liechtenstein/du.txt
-       du    5      4793    3
-    */
+  Successfully parsed liechtenstein/du.txt
+    du    5      4793    3
+  */
 }
 #endif
