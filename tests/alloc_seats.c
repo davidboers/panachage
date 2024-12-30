5b7fff129cfab3d8ac61d4f2bb2a24d340bcc750
davidboers <kwmmineccraft@gmail.com>
2024-12-30 14:44:11 -0500

    Constructor now requires id parameter

--- tests/alloc_seats.c
+++ tests/alloc_seats.c
@@ -4,11 +4,11 @@
 void test_lrm(void)
 {
     party parties[] = {
-        party_new(613), // 62
-        party_new(323), // 32
-        party_new(43),  // 4
-        party_new(13),  // 1
-        party_new(8)};  // 1
+        party_new(1, 613), // 62
+        party_new(2, 323), // 32
+        party_new(3, 43),  // 4
+        party_new(4, 13),  // 1
+        party_new(5, 8)};  // 1
     lrm(5, parties, 100);
     lrm_print_table(5, parties);
 }
@@ -16,11 +16,11 @@ void test_lrm(void)
 void test_ham(void)
 {
     party parties[] = {
-        party_new(613), // 66
-        party_new(323), // 34
-        party_new(43),
-        party_new(13),
-        party_new(8)};
+        party_new(1, 613), // 66
+        party_new(2, 323), // 34
+        party_new(3, 43),
+        party_new(4, 13),
+        party_new(5, 8)};
     int partyc = 5;
     partyc = threshold(partyc, parties, 0.05);
     ham(partyc, parties, 100, dhondt);
